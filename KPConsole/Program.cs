207a174c646f4f49f2e69bb1912fe3ef1f1d74a7
KPasumarthy <KPasumarthy@gmail.com>
2020-01-01 15:50:11 +0000

    KP : KPWebAPIs configured on IIS

---
+++ KPConsole/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Reflection;
+
+
+namespace KPConsole
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("KP : Hello World!");
+
+            ///KP : Question : 1
+            int a, b = 0;
+            a = 5;
+            b += a;     //b = b+a; b = 0+5 = 5
+            Console.WriteLine("Opeartion : b += a");
+            Console.WriteLine("a = " + a);
+            Console.WriteLine("b = " + b);
+
+            ///KP : Question : 2
+            
+
+            Console.ReadLine();
+        }
+    }
+}
