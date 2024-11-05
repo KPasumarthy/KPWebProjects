857d8c3ab1782dabc97f0374e2b6c1ccd4cd6aef
Kailash Pasumarthy <KPasumarthy@gmail.com>
2024-11-05 12:29:43 -0500

    KP : Today's Date as a String!

--- KPConsole/Program.cs
+++ KPConsole/Program.cs
@@ -15,23 +15,18 @@ namespace KPConsole
         {
             Console.WriteLine("KP : Hello Kailash World!");
 
-            ///*/////KP : Question : PrintFizzBuzz
-            //Int64 n = 15;
-            ////PrintFizzBuzz(n);
-            //FizzBuzz.PrintFizzBuzz(n);
-            //FizzBuzz.PrintFizzBuzzComplex(n);
-            ///////KP : Question : PrintFizzBuzz///*/
-
-            ///Question : ????
+            ///Question
             ///
 
-            String input = "{{{{(}}]]]}}{{}}}";
+            // Getting the string form of the current date
+            // in a format, i.e, 12/04/2021 07:12 PM          
+            string currentDate = DateTime.Now.ToString("MM/dd/yyyy hh:mm tt");
+            String input = "KP: Today's Date : " + currentDate;
             bool output = TestFunction(input);
 
             ////KP : Console Wait to ReadLine
             Console.ReadLine();
         }
-
 
         static bool TestFunction(String input)
         {
@@ -39,9 +34,16 @@ namespace KPConsole
             Console.WriteLine(input);
 
             return true;
-
         }
-
 
     }
 }
+
+
+
+///*/////KP : Question : PrintFizzBuzz
+//Int64 n = 15;
+////PrintFizzBuzz(n);
+//FizzBuzz.PrintFizzBuzz(n);
+//FizzBuzz.PrintFizzBuzzComplex(n);
+///////KP : Question : PrintFizzBuzz///*/
