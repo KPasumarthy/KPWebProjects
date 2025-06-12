7ffb64c8ec75ba13410616d83de95acdb78079e5
Kailash <KPasumarthy@gmail.com>
2025-06-12 15:41:28 -0400

    KP : KPMVCWebAPIs New REST APIs added!

---
+++ KPMVCWebAPIs/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace KPMVCWebAPIs.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
--- KPMVCWebAPIs/Program.cs
+++ KPMVCWebAPIs/Program.cs
@@ -1,38 +1,51 @@
-using Microsoft.AspNetCore.Authentication;
-using Microsoft.AspNetCore.Authentication.JwtBearer;
-using Microsoft.Identity.Web;
-
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
-builder.Services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
-    .AddMicrosoftIdentityWebApi(builder.Configuration.GetSection("AzureAd"));
-
-builder.Services.AddControllers();
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
-builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
+builder.Services.AddControllersWithViews();
 
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
+if (!app.Environment.IsDevelopment())
 {
-    app.UseSwagger();
-    app.UseSwaggerUI();
+    app.UseExceptionHandler("/Home/Error");
+    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+    app.UseHsts();
 }
 
-//app.UseHttpsRedirection();
+app.UseHttpsRedirection();
+app.UseStaticFiles();
 
+app.UseRouting();
 
-//app.UseAuthorization();
+app.UseAuthorization();
 
-//app.MapControllers();
-//app.UseMvc(routes =>
-//{
-//    routes.MapRoute(
-//        name: "default",
-//        template: "{controller=WeatherForecast}/{action=Index}/{id?}");
-//});
+app.MapControllerRoute(
+    name: "default",
+    pattern: "{controller=Home}/{action=Index}/{id?}");
+
+//app.MapGet("/todoitems", async (TodoDb db) =>
+//    await db.Todos.ToListAsync());
+
+//app.MapGet("/todoitems/complete", async (TodoDb db) =>
+//    await db.Todos.Where(t => t.IsComplete).ToListAsync());
+
+//app.MapGet("/todoitems/{id}", async (int id, TodoDb db) =>
+//    await db.Todos.FindAsync(id)
+//        is Todo todo
+//            ? Results.Ok(todo)
+//            : Results.NotFound());
+
+//app.MapGet("/", () => "KP : Hello World!");
+app.MapGet("/Value", () => "KP : KPMVCWebAPIs :  Hello World!");
+
+// Getting the string form of the current date
+// in a format, i.e, 07/27/2024 07:29:00 AM          
+string currentDate = DateTime.Now.ToString("MM/dd/yyyy HH:mm:sss tt");
+String input = "KP : Today's Date : " + currentDate;
+Console.WriteLine("KP : KPMVCWebAPIs ! : Today's Date : " + currentDate);
+
+app.MapGet("/DateTimeNow", () => "KP : KPMVCWebAPIs : DateTimeNow : " + currentDate);
+
 
 app.Run();
